44449056e8c56f092511ab7dd6dd8e3445635f59
Nananilda <anna.frezarini@aluno.senai.br>
2025-09-04 07:22:13 -0300

    correção

--- 20250821/while.php
+++ 20250821/while.php
@@ -8,7 +8,7 @@
     //do while, executa e depois faz a lógica
     $contador = 1
         do{
-            echo "numero: contador<br>";
+            echo "numero: $contador<br>";
             $contador++;
         }while ($contador <=5);
 ?>
